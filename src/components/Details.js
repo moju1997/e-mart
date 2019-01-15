803c0ad3bb4834e81b5421b0c728eb597102f55a
Mojahid <hussainmojahid3@gmail.com>
2019-01-15 10:54:57 +0530

    Second Commit

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { ProductConsumer } from '../context';
+import { Link } from 'react-router-dom';
+import { ButtonContainer } from './Button';
 
-export default class  extends Component {
+export default class extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Detail</h3>
-      </div>
+      <ProductConsumer>
+        {value => {
+          const { id, company, img, info, price, title, inCart } = value.detailProduct;
+          return (
+            <div className='container py-5'>
+              {/* Title */}
+              <div className='row'>
+                <div className='col-10 mx-auto text-center text-slanted text-blue my-5'>
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/* End Title */}
+              {/* Product Info */}
+              <div className='row'>
+                <div className='col-10 mx-auto col-md-6 my-3 '>
+                  <img src={img} className='img-fluid' alt='product' />
+                </div>
+                {/* product text */}
+                <div className='col-10 mx-auto col-md-6 my-3 text-capitalize'>
+                  <h2>model: {title}</h2>
+                  <h4 className='text-title text-uppercase text-muted mt-3 mb-2'>manufactured by:
+                   <span className='text-uppercase '>{company}</span>
+                  </h4>
+                  <h4 className='text-blue'>
+                    <strong>
+                      price: <span>Rs.</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className='text-capitalize font-weight-bold mt-3 mb-0'>
+                    info about the Product
+                  </p>
+                  <p className='text-muted lead'>
+                    {info}
+                  </p>
+                  {/* button */}
+                  <div>
+                    <Link to='/'>
+                    <ButtonContainer>
+                      back to product 
+                    </ButtonContainer>
+                    </Link>
+                    <ButtonContainer  cart disabled={inCart?true:false} onClick={()=>{
+                      value.addToCart(id);
+                    }}>
+                      {inCart?'inCart':'add to cart'}
+                    </ButtonContainer>
+                  </div>
+                </div>
+              </div>
+              {/* End Product infor */}
+            </div>
+          )
+        }}
+      </ProductConsumer>
     )
   }
 }
--- src/context.js
+++ src/context.js
@@ -6,15 +6,41 @@ const ProductContext=React.createContext()
 
 class ProductProvider extends Component {
     state={
-        products:storeProducts,
+        products:[],
         detailProduct:detailProduct
+    }
+    componentDidMount(){
+      this.setProducts();
+    }
+    setProducts=()=>{
+      let products=[];
+      storeProducts.forEach(item=>{
+        const singleItem={...item};
+        products=[...products,singleItem]
+      })
+      this.setState(()=>{
+        return{products:products}
+      })
     }
     handleDetail =()=>{
         console.log('hello from details')
     }
-    addToCart =()=>{
-        console.log('hello from add to cart')
+    addToCart =id=>{
+        console.log(`hello from add to cart ${id}`)
     }
+    // tester=()=>{
+    //   console.log('the state product  is:',this.state.products[0].inCart);
+    //   console.log('the data product  is:',storeProducts[0].inCart);
+
+    //   const tempProducts=[...this.state.products];
+    //   tempProducts[0].inCart=true;
+    //   this.setState(()=>{
+    //     return{products:tempProducts}
+    //   },()=>{
+    //     console.log('the state product  is:',this.state.products[0].inCart);
+    //   console.log('the data product  is:',storeProducts[0].inCart); 
+    //   })
+    // }
   render() {
     return (
       <ProductContext.Provider value={{
@@ -22,6 +48,7 @@ class ProductProvider extends Component {
         handleDetail:this.handleDetail,
         addToCart:this.addToCart
       }}>
+      {/* <button onClick={this.tester}>test me</button> */}
         {this.props.children}
       </ProductContext.Provider>
     )
