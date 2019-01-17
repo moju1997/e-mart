c2ac492969ffd7dc189be57658bfed2ea2ee570d
Mojahid <hussainmojahid3@gmail.com>
2019-01-18 01:59:47 +0530

    Final 2 Commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Cart from './components/cart/Cart';
 import Default from './components/Default';
 import Details from './components/Details';
 import Modal from './components/Modal';
+import FooterPage from './components/FooterPage';
 
 class App extends React.Component {
   render() {
@@ -22,7 +23,9 @@ class App extends React.Component {
          <Route  component={Default}/>
          
        </Switch>
+       <FooterPage />
        <Modal />
+       
       </React.Fragment>
     );
   }
